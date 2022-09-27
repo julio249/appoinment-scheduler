3c3519a2ea5d089671814fb3921eed0f8571f3df
julio249 <63440967+julio249@users.noreply.github.com>
2022-09-27 11:53:52 -0500

    Add files via upload

---
+++ Appoinments/Main.java
@@ -0,0 +1,11 @@
+import static ui.UIMenu.*;
+// import static ui.UIDoctorMenu;
+// import static ui.UIPatientMenu;
+
+public class Main {
+    public static void main(String[] args) {
+        showMenu();
+        // UIMenu.showMenu();
+    }
+
+}
